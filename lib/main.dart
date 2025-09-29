fcb63bc58fee1fa06b71642aca7a758ab928ad03
Mebibora <mebiboraakerejola@gmail.com>
2025-09-29 17:40:21 -0400

    created display

--- lib/main.dart
+++ lib/main.dart
@@ -86,37 +86,105 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Container(
+              width: 200,
+              height: 100,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black,
+                  width: 2),
+              )
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {
+
+                  }, 
+                child: Text("7")),
+                ElevatedButton(
+                  onPressed: () {
+
+                  }, 
+                child: Text("8")),
+                ElevatedButton(
+                  onPressed: () {
+
+                  }, 
+                child: Text("9")),
+
+              ],
+            ),
+           Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {
+
+                  }, 
+                child: Text("4")),
+                ElevatedButton(
+                  onPressed: () {
+
+                  }, 
+                child: Text("5")),
+                ElevatedButton(
+                  onPressed: () {
+
+                  }, 
+                child: Text("6")),
+
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {
+
+                  }, 
+                child: Text("1")),
+                ElevatedButton(
+                  onPressed: () {
+
+                  }, 
+                child: Text("2")),
+                ElevatedButton(
+                  onPressed: () {
+
+                  }, 
+                child: Text("3")),
+
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {
+
+                  }, 
+                child: Text("0")),
+                ElevatedButton(
+                  onPressed: () {
+
+                  }, 
+                child: Text("<-")),
+                ElevatedButton(
+                  onPressed: () {
+
+                  }, 
+                child: Text("enter")),
+
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
